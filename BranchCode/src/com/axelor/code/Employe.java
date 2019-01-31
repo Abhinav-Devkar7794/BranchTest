e9b62dd04901514343144faa0704f55e9cae06f8
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-31 22:42:23 +0530

    Address Method

---
+++ BranchCode/src/com/axelor/code/Employe.java
@@ -0,0 +1,14 @@
+package com.axelor.code;
+
+public class Employe {
+
+	void address() {
+		System.out.println("Employee Address !!");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
