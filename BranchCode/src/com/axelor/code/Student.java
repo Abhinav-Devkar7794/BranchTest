a6399efd5b31e03ccb272e8721b2f0524fdf3ffa
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-31 21:49:59 +0530

    Course Added

--- BranchCode/src/com/axelor/code/Student.java
+++ BranchCode/src/com/axelor/code/Student.java
@@ -7,6 +7,14 @@ public class Student {
 
 		
 		System.out.println("Abhinav Devkar");
+		
+		System.out.println("Courses :");
+		System.out.println("JPA");
+		System.out.println("Google Guice");
+		System.out.println("Hibernate");
+		System.out.println("Postgresql");
+		System.out.println("ADK");
+		System.out.println("Git");
 	}
 
 }
